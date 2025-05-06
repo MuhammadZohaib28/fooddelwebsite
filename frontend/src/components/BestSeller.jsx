e01e363920b23770229eec5c34ca4bfc9cac9cc6
Muhammad Zohaib <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-05-06 14:49:35 +0500

    Main Banner Bottom Banner Footer Newsletter
    sections created

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -8,8 +8,13 @@ const BestSeller = () => {
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Best Seller</p>
 
-      <div>
-        <ProductCard product={products[0]} />
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 md:gap-6 lg:grid-cols-5 mt-5">
+        {products
+          .filter((product) => product.inStock)
+          .slice(0, 5)
+          .map((i, index) => (
+            <ProductCard key={index} product={i} />
+          ))}
       </div>
     </div>
   );
