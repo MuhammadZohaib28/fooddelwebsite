f18842329ccee16d11a53d598b9bab7f13f5a7b0
Muhammad Zohaib <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-05-08 16:45:15 +0500

    Project Detail page created, and products naviagted .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { useAppContext } from "./context/AppContext";
 import Login from "./components/Login";
 import AllProducts from "./pages/AllProducts";
 import ProductCategory from "./pages/ProductCategory";
+import ProjectDetails from "./pages/ProjectDetails";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -26,6 +27,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/products/" element={<AllProducts />} />
           <Route path="/products/:category" element={<ProductCategory />} />
+          <Route path="/products/:category/:id" element={<ProjectDetails />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
